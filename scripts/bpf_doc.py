fb964ee9a3cc3eac2a04e57c1d6d35ca670cb8ee
Harsh Modi <harshmodi@google.com>
2022-07-19 16:32:50 -0700

    bpf: Add bpf_[skb|xdp]_packet_hash.
    eBPF currently does not have a good way to support more advanced
    checksums like crc32c checksums. A bpf helper that allows users
    to hash packet data from eBPF will use this.
    
    Currently, it only supports crc32c, however, additional support for
    new hashes can be supported by adding an additional enum and
    implementing the corresponding code in net/core/filter.c.
    
    Signed-off-by: Harsh Modi <harshmodi@google.com>

--- scripts/bpf_doc.py
+++ scripts/bpf_doc.py
@@ -576,6 +576,7 @@ class PrinterHelpers(Printer):
 
     type_fwds = [
             'struct bpf_fib_lookup',
+            'struct bpf_packet_hash_params',
             'struct bpf_sk_lookup',
             'struct bpf_perf_event_data',
             'struct bpf_perf_event_value',
@@ -633,6 +634,7 @@ class PrinterHelpers(Printer):
             '__wsum',
 
             'struct bpf_fib_lookup',
+            'struct bpf_packet_hash_params',
             'struct bpf_perf_event_data',
             'struct bpf_perf_event_value',
             'struct bpf_pidns_info',
